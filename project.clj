e9f850f003c8eedbab1fc5b84ed34eb56a604ed2
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-02-21 01:44:52 +0100

    Bump CIDER version dependency

--- project.clj
+++ project.clj
@@ -31,4 +31,4 @@
                      [lein-cljfmt "0.6.2"]]}
    :repl {:repl-options {:host "0.0.0.0"
                          :port 4001}
-          :plugins [[cider/cider-nrepl "0.18.0"]]}})
+          :plugins [[cider/cider-nrepl "0.20.0"]]}})
