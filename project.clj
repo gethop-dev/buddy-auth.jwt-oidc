ff29996fc772c4bdb79c9d50b1617e9a34c5680f
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2020-03-19 21:20:40 +0100

    Prepare for next development release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject magnet/buddy-auth.jwt-oidc "0.8.5"
+(defproject magnet/buddy-auth.jwt-oidc "0.8.6-SNAPSHOT"
   :description "Buddy-auth JWT token validator, for OpenID Connect ID tokens"
   :url "https://github.com/magnetcoop/buddy-auth.jwt-oidc"
   :min-lein-version "2.9.0"
